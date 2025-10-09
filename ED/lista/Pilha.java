6d39779186d7f440132d6f213e055d972d0fbad1
Giullia Ladislau <151695932+giuladislau@users.noreply.github.com>
2025-10-09 22:24:13 +0000

    adicionando exercício 1

---
+++ ED/lista/Pilha.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Pilha<T> {
+    private ArrayList<T> elementos;
+    
+    public Pilha() {
+        elementos = new ArrayList<>();
+    }
+    
+    public void empilhar(T elemento) {
+        elementos.add(elemento);
+    }
+    
+    public T desempilhar() {
+        if (isEmpty()) {
+            return null;
+        }
+        return elementos.remove(elementos.size() - 1);
+    }
+    
+    public boolean isEmpty() {
+        return elementos.isEmpty();
+    }
+}
